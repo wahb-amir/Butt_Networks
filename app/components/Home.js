d48d92986eaef1695038e4345eeeb7e0476f2eda
ShahanwazSaddam144 <shahnawazsaddamb@gmail.com>
2025-09-23 14:10:36 +0500

    commit

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,38 +1,13 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const Home_ = () => {
-  const [showPopup, setShowPopup] = useState(true); 
-
-  useEffect(() => {
-    const timer = setTimeout(() => setShowPopup(false), 8000);
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
     <section
       id="home"
       className="Home py-20 px-5 bg-gradient-to-br from-gray-500 to-gray-400 mt-19 cursor-pointer"
     >
-      {/* POPUP MODAL */}
-      {showPopup && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50 backdrop-blur-sm z-50">
-          <div className="bg-white rounded-2xl shadow-2xl p-6 w-[90%] max-w-md text-center animate-fadeIn">
-            <h2 className="text-2xl font-bold text-gray-800 mb-3">
-              👋 Welcome to Butt Networks
-            </h2>
-            <p className="text-gray-600 mb-5">
-           We are proud to be collaborating with <b>Chohan Space</b>, led by CEO Abdullah Chohan.
-            </p>
-            <button
-              onClick={() => setShowPopup(false)}
-              className="px-5 py-2 bg-gradient-to-r from-gray-800 to-gray-900 text-white rounded-xl hover:scale-105 transition shadow-lg"
-            >
-              Continue
-            </button>
-          </div>
-        </div>
-      )}
 
       {/* MAIN CONTENT */}
       <div
