727aeac3d6bd972e9da81e112070a3025f731637
ShahanwazSaddam144 <shahnawazsaddamb@gmail.com>
2025-12-19 21:09:22 +0500

    commit all

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -76,7 +76,7 @@ const Navbar = () => {
           </li>
           <li>
             <Link
-              href="/contact"
+              href="/Contact"
               className="flex items-center gap-2 hover:text-yellow-300 hover:bg-gray-500 dark:hover:bg-gray-700 px-4 py-2 rounded font-semibold"
             >
               <Phone size={18} /> Contact
